d0e09b620e126af58145bfdff79ad52deb5e4eab
Stone Yang <stoneyang17@gmail.com>
2024-01-02 16:31:32 -0500

    Edits

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_app.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'personal_website.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- personal_website/settings.py
+++ personal_website/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'vercel_app.urls'
+ROOT_URLCONF = 'personal_website.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'vercel_app.wsgi.app'
+WSGI_APPLICATION = 'personal_website.wsgi.app'
 
 
 # Database
