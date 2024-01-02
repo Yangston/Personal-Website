6564b7faa2906277c9e32747518e8e8615184bd0
Stone Yang <stoneyang17@gmail.com>
2024-01-02 14:32:07 -0500

    Update settings.py

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app',
+                 'django-template-yangstons-projects.vercel.app']
 
 
 # Application definition
@@ -115,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
