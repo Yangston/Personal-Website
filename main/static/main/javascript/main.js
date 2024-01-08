4f5d3152635cde6bbfa2b00b926eec12406ecd86
Stone Yang <stoneyang17@gmail.com>
2024-01-07 20:09:33 -0500

    Completed Skeleton + Responsive CSS
    - Main website skeleton done
    - Preliminary responsive css

--- main/static/main/javascript/main.js
+++ main/static/main/javascript/main.js
@@ -1,6 +1,8 @@
 var tabLinks = document.getElementsByClassName("about-tabs");
 var tabContents = document.getElementsByClassName("tab-contents");
 
+var menu = document.getElementById("menu")
+
 function selectTab(tabName){
     for(tabLink of tabLinks){
         tabLink.classList.remove("active-tab")
@@ -11,3 +13,12 @@ function selectTab(tabName){
     event.currentTarget.classList.add("active-tab")
     document.getElementById(tabName).classList.add("active-content")
 }
+
+function openMenu(){
+    menu.style.right = "0";
+}
+
+function closeMenu(){
+    menu.style.right = "-200px";
+}
+
